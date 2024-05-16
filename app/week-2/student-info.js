1bcb97c3ea2efcfc01549a5b8bc3bbba7ab1c195
Prayus04 <prayus.sharma@edu.sait.ca>
2024-05-16 11:07:27 -0600

    week 2 assignment
    Completed the week 2 page, also deleted and recreated the main page.

---
+++ app/week-2/student-info.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+
+export default function StudentInfo(){
+    
+    return(
+
+        <div>
+            <h2>Prayus Sharma</h2>   
+            <Link href="https://github.com/Prayus04">https://github.com</Link>
+        </div>
+    );
+
+}
